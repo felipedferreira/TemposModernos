705ed0df7567228819d10ab9bc0c985d3be72c1b
felipe d ferreira <felipedferreira11@gmail.com>
2026-04-05 23:39:17 -0400

    refactor: replace minimal API with traditional controller-based routing

---
+++ src/Presentation/Acme.Cinedex.MoviesService.WebService/Controllers/MoviesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Acme.Cinedex.MoviesService.WebService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MoviesController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok("Hello from MoviesController");
+}
--- src/Presentation/Acme.Cinedex.MoviesService.WebService/Program.cs
+++ src/Presentation/Acme.Cinedex.MoviesService.WebService/Program.cs
@@ -11,6 +11,8 @@ public static class Program
         // Add services to the container.
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
+        
+        builder.Services.AddControllers();
 
         var app = builder.Build();
 
@@ -22,24 +24,7 @@ public static class Program
 
         app.UseHttpsRedirection();
 
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", () =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                        new WeatherForecast
-                        (
-                            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            Random.Shared.Next(-20, 55),
-                            summaries[Random.Shared.Next(summaries.Length)]
-                        ))
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
+        app.MapControllers();
 
         await app.RunAsync();
     }
